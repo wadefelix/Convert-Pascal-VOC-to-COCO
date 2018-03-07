a652243c67de1bd69b97d8b04f88fedf407b6463
merge <merge@mergesecurity.com>
2018-03-07 14:46:12 +0800

    origin pascal classes, using xml.etree.ElementTree

--- convertVOC2COCO.py
+++ convertVOC2COCO.py
@@ -1,118 +1,90 @@
+#! python
 import os
 import xml.etree.ElementTree as ET
-import xmltodict
 import json
-from xml.dom import minidom
 from collections import OrderedDict
 
 
-#attrDict = {"images":[{"file_name":[],"height":[], "width":[],"id":[]}], "type":"instances", "annotations":[], "categories":[]}
-
-#xmlfile = "000023.xml"
-
+CATEGORIES = [{"supercategory":"none","id":1,"name":"aeroplane"},
+              {"supercategory":"none","id":2,"name":"bicycle"},
+              {"supercategory":"none","id":3,"name":"bird"},
+              {"supercategory":"none","id":4,"name":"boat"},
+              {"supercategory":"none","id":5,"name":"bottle"},
+              {"supercategory":"none","id":6,"name":"bus"},
+              {"supercategory":"none","id":7,"name":"car"},
+              {"supercategory":"none","id":8,"name":"cat"},
+              {"supercategory":"none","id":9,"name":"chair"},
+              {"supercategory":"none","id":10,"name":"cow"},
+              {"supercategory":"none","id":11,"name":"diningtable"},
+              {"supercategory":"none","id":12,"name":"dog"},
+              {"supercategory":"none","id":13,"name":"horse"},
+              {"supercategory":"none","id":14,"name":"motorbike"},
+              {"supercategory":"none","id":15,"name":"person"},
+              {"supercategory":"none","id":16,"name":"pottedplant"},
+              {"supercategory":"none","id":17,"name":"sheep"},
+              {"supercategory":"none","id":18,"name":"sofa"},
+              {"supercategory":"none","id":19,"name":"train"},
+              {"supercategory":"none","id":20,"name":"tvmonitor"}]
 
 def generateVOC2Json(rootDir,testXMLFiles):
-	attrDict = dict()
-	#images = dict()
-	#images1 = list()
-	#class name in the images.
-	attrDict["categories"]=[{"supercategory":"none","id":1,"name":"header"},
-			        		{"supercategory":"none","id":2,"name":"row"},
-			        		{"supercategory":"none","id":3,"name":"logo"},
-			        		{"supercategory":"none","id":4,"name":"item_name"},
-							{"supercategory":"none","id":5,"name":"item_desc"},
-							{"supercategory":"none","id":6,"name":"price"},
-							{"supercategory":"none","id":7,"name":"total_price_text"},
-							{"supercategory":"none","id":8,"name":"total_price"},
-							{"supercategory":"none","id":9,"name":"footer"}
-							]
-	images = list()
-	annotations = list()
-	for root, dirs, files in os.walk(rootDir):
-		for file in testXMLFiles:
-			if file in files:
+    attrDict = dict()
+    attrDict["categories"] = CATEGORIES
+    images = list()
+    annotations = list()
+    for rootdir, dirs, files in os.walk(rootDir):
+        for file in testXMLFiles:
+            if file in files:
+                annotation_path = os.path.join(rootdir, file)
+                
+                tree = ET.parse(annotation_path)
+                root = tree.getroot()
+                image = dict()
+                image['file_name'] = root.find('filename').text
+                image['height'] = int(root.find('size').find('height').text)
+                image['width'] = int(root.find('size').find('width').text)
 
-				annotation_path = os.path.abspath(os.path.join(root, file))
-				
-				#tree = ET.parse(annotation_path)#.getroot()
-				image = dict()
-				#keyList = list()
-				doc = xmltodict.parse(open(annotation_path).read())
-				#print doc['annotation']['filename']
-				image['file_name'] = str(doc['annotation']['filename'])
-				#keyList.append("file_name")
-				image['height'] = int(doc['annotation']['size']['height'])
-				#keyList.append("height")
-				image['width'] = int(doc['annotation']['size']['width'])
-				#keyList.append("width")
+                image['id'] = image['file_name'].split('.jpg')[0]
+                images.append(image)
+                objs = tree.findall('object')
+                id1 = 1
+                for obj in objs:
+                        for value in attrDict["categories"]:
+                            annotation = dict()
+                            if obj.find('name').text in value["name"]:
+                                annotation["segmentation"] = []
+                                annotation["iscrowd"] = 0
+                                annotation["image_id"] = image['id']
+                                bbox = obj.find('bndbox')
+                                x1 = int(bbox.find("xmin").text) - 1
+                                y1 = int(bbox.find("ymin").text) - 1
+                                x2 = int(bbox.find("xmax").text) - x1
+                                y2 = int(bbox.find("ymax").text) - y1
+                                annotation["bbox"] = [x1, y1, x2, y2]
+                                annotation["area"] = int(x2 * y2)
+                                annotation["category_id"] = value["id"]
+                                annotation["ignore"] = 0
+                                annotation["id"] = id1
+                                id1 +=1
 
+                                annotations.append(annotation)
 
+    attrDict["images"] = images    
+    attrDict["annotations"] = annotations
+    attrDict["type"] = "instances"
 
+    jsonString = json.dumps(attrDict)
+    with open("voc_2012_train.json", "w") as f:
+        f.write(jsonString)
 
-				image['id'] = str(doc['annotation']['filename']).split('.jpg')[0]
-				images.append(image)
-				# keyList.append("id")
-				# for k in keyList:
-				# 	images1.append(images[k])
-				# images2 = dict(zip(keyList, images1))
-				# print images2
-				#print images
-
-				
-
-
-				
-				#attrDict["images"] = images
-
-				#print attrDict
-				#annotation = dict()
-				id1 = 1
-				if 'object' in doc['annotation']:
-					for obj in doc['annotation']['object']:
-						for value in attrDict["categories"]:
-							annotation = dict()
-							if str(obj['name']) in value["name"]:
-								#print str(obj['name'])
-								annotation["segmentation"] = []
-								annotation["iscrowd"] = 0
-								annotation["image_id"] = str(doc['annotation']['filename']).split('.jpg')[0] #attrDict["images"]["id"]
-								x1 = int(obj["bndbox"]["xmin"]) - 1
-								y1 = int(obj["bndbox"]["ymin"]) - 1
-								x2 = int(obj["bndbox"]["xmax"]) - x1
-								y2 = int(obj["bndbox"]["ymax"]) - y1
-								annotation["bbox"] = [x1, y1, x2, y2]
-								annotation["area"] = int(x2 * y2)
-								annotation["category_id"] = value["id"]
-								annotation["ignore"] = 0
-								annotation["id"] = id1
-								id1 +=1
-
-								annotations.append(annotation)
-
-	attrDict["images"] = images	
-	attrDict["annotations"] = annotations
-	attrDict["type"] = "instances"
-
-	#print attrDict
-	jsonString = json.dumps(attrDict)
-	with open("voc_train.json", "w") as f:
-		f.write(jsonString)
-
-# rootDir = "./OBJECT_DETECTION/detectron/lib/datasets/data/VOC/Annotations"
-# for root, dirs, files in os.walk(rootDir):
-# 	for file in files:
-# 		if file.endswith(".xml"):
-# 			annotation_path = str(os.path.abspath(os.path.join(root,file)))
-# 			#print(annotation_path)
-# 			generateVOC2Json(annotation_path)
-trainFile = "./OBJECT_DETECTION/train.txt"
+trainFile = os.path.expanduser("~/data/VOCdevkit/VOC2012/ImageSets/Main/train.txt")
 trainXMLFiles = list()
 with open(trainFile, "rb") as f:
-	for line in f:
-		fileName = line.strip()
-		print fileName
-		trainXMLFiles.append(fileName + ".xml")
+    for line in f:
+        fileName = line.strip()
+        print fileName
+        trainXMLFiles.append(fileName + ".xml")
 
 
-rootDir = "./OBJECT_DETECTION/detectron/lib/datasets/data/VOC/Annotations_xml"
+rootDir = os.path.expanduser("~/home/merge/data/VOCdevkit/VOC2012/Annotations/")
 generateVOC2Json(rootDir, trainXMLFiles)
+
